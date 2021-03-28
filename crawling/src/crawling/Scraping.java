ea51fb0fac6b87ef854e9f677546ead1556a9c9f
kgggh <68227318+kgggh@users.noreply.github.com>
2021-03-28 21:06:34 +0900

    크롤링 수정

--- crawling/src/crawling/Scraping.java
+++ crawling/src/crawling/Scraping.java
@@ -27,27 +27,14 @@ public class Scraping {
 		List<KoreaStats> koreaStatsList = new ArrayList<>();
         Document doc = Jsoup.connect(KOREA_COVID_DATAS_URL).get();
         Elements contents = doc.select("table tbody tr");
-        for(Element content : contents){
-            Elements tdContents = content.select("td");
-
-            KoreaStats koreaStats = KoreaStats.builder()
-                    .country(content.select("th").text())
-                    .diffFromPrevDay((tdContents.get(0).text()))
-                    .total((tdContents.get(1).text()))
-                    .death((tdContents.get(2).text()))
-                    .incidence((tdContents.get(3).text()))
-                    .inspection((tdContents.get(4).text()))
-                    .build();
-            koreaStatsList.add(koreaStats);
-        }
-		System.out.println(koreaStatsList);
 		
 		Date date = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.KOREA);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH_mm_ss", Locale.KOREA);
 		String today = sdf.format(date);
+		System.out.println(today);
 		
-		String filePath = "/Users/gimgeonhui/Desktop/test/";
-		String fileName = "log"+today+".csv";
+		String filePath = "/Users/kimgeonhee/Desktop/test/";
+		String fileName = "test "+today+".csv";
 		String makePath = filePath + File.separator + fileName;
 		String NEWLINE = System.lineSeparator(); // 줄바꿈(\n)
 		 
@@ -57,18 +44,35 @@ public class Scraping {
 		}
 		 
 		try {
-		    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(makePath), "EUC-KR"));
-		    bw.write("번호,이름,지역");
-			bw.write(NEWLINE);
-			
-			bw.write("1,김철수,서울");
-			bw.write("\n");
-			
-			bw.write("2,김영희,경기");
-			bw.write("\r\n");
-			
-			bw.write("3,이철희,경북");
-			
+		    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(makePath), "UTF-8"));
+		    bw.write("지역,국내발생,해외유입,확진환자,격리환자");
+		    bw.write(NEWLINE);			
+			 for(Element content : contents){
+	            Elements tdContents = content.select("td");
+	            bw.write(content.select("th").text().replace(",", "."));
+	        	bw.write(",");
+				bw.write(tdContents.get(0).text().replace(",", "."));
+				bw.write(",");
+				bw.write(tdContents.get(1).text().replace(",", "."));
+				bw.write(",");
+				bw.write(tdContents.get(2).text().replace(",", "."));
+				bw.write(",");
+				bw.write(tdContents.get(3).text().replace(",", "."));
+				bw.write(",");
+				bw.write(tdContents.get(4).text().replace(",", "."));
+				bw.write("\n");
+	            KoreaStats koreaStats = KoreaStats.builder()
+	                    .country(content.select("th").text())
+	                    .diffFromPrevDay((tdContents.get(0).text()))
+	                    .total((tdContents.get(1).text()))
+	                    .death((tdContents.get(2).text()))
+	                    .incidence((tdContents.get(3).text()))
+	                    .inspection((tdContents.get(4).text()))
+	                    .build();
+	            koreaStatsList.add(koreaStats);
+	            System.out.println(content.text());
+	            System.out.println(koreaStatsList);
+	        }
 			bw.flush();
 			bw.close();
 		} catch (IOException e) {
